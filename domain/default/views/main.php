3367824787b3151fe8f529d6be7a3a5207db1bd5
Vitaliy Olkhin <ovvitalik@gmail.com>
2022-06-21 18:02:53 +0500

    Добавлены компоненты css и js bootstrap-4.6.1, jquery-3.6.0. Добавлена favicon.ico по умолчанию

--- domain/default/views/main.php
+++ domain/default/views/main.php
@@ -9,15 +9,11 @@ use wco\kernel\WCO;
         $this->Seo();
         ?>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="<?=WCO::$config['protocol'].$_SERVER['SERVER_NAME']?>/template/default/img/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="/template/default/libre/bootstrap-4.4.1-dist/css/bootstrap.css">
-        <link rel="stylesheet" href="/template/default/libre/font-awesome-4.7.0/css/font-awesome.css">
-        <link rel="stylesheet" href="/template/default/css/style.css?v=2">
-        <link rel="stylesheet" href="/template/default/css/mobail_menu.css">
-        <script src="https://yandex.st/jquery/2.1.1/jquery.min.js"></script>
+        <link rel="icon" href="<?=WCO::$config['protocol'].$_SERVER['SERVER_NAME']?>/default/favicon.ico" type="image/x-icon">
     </head>
     <body>
         <?=$this->views?>
+        <?=WCO::getBodyEnd()?>
     </body>
 </html>
 
