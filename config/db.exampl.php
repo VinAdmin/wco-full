96351b52a7be16ba05b8f97ec4d1dcab0c419178
Vitaliy Olkhin <ovvitalik@gmail.com>
2022-06-27 15:36:37 +0500

    шаблон конфига для бд

---
+++ config/db.exampl.php
@@ -0,0 +1,19 @@
+<?php
+$config_db = [
+    'default' => [
+        'db' => 'mysql',
+        'host' => 'localhost',
+        'db_name' => '',
+        'login' => '',
+        'password' => ''
+    ],
+    /*
+    'dbconnect2' => [
+        'db' => 'mysql',
+        'host' => 'localhost',
+        'db_name' => '',
+        'login' => '',
+        'password' => ''
+    ],*/
+];
+?>
